dec5418c95b89988a9204c1cd6d59bec064024a6
漱棠 <shibinghua.sbh@alibaba-inc.com>
2023-12-05 21:18:32 +0800

    提交生成二维码功能

---
+++ go.mod
@@ -0,0 +1,7 @@
+module serverless-demo
+
+go 1.17
+
+require github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+
+require github.com/aliyun/fc-runtime-go-sdk v0.2.7 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/aliyun/fc-runtime-go-sdk/fc"
+	qrcode "github.com/skip2/go-qrcode"
+)
 
 func main() {
 	fmt.Println("Hello Serverless World!")
+
+	png, err := qrcode.Encode("opt.Content", qrcode.Medium, 256)
+	fmt.Println(png, err)
+
+	fc.StartHttp(HandleGenQRCode)
+}
+
+type AliyunOssConf struct {
+	Cname        string `json:"cname"`
+	EndPoint     string `json:"endpoint"`
+	AccessKey    string `json:"accessKey"`
+	AccessSecret string `json:"accesssSecret"`
+	Bucket       string `json:"bucket"`
+}
+
+func HandleGenQRCode(ctx context.Context, w http.ResponseWriter, req *http.Request) error {
+
+	content := req.FormValue("content")
+	png, err := qrcode.Encode(content, qrcode.Medium, 256)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Header().Add("Content-Type", "text/plain")
+		w.Write([]byte(err.Error()))
+		return nil
+	}
+
+	fmt.Println(png)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "text/plain")
+	w.Write(png)
+	return nil
+
 }
